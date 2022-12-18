7a63249a41ce9ee39c6126e529de718d428d31d7
Zakary2841 <26402356+zakary2841@users.noreply.github.com>
2022-12-18 05:35:43 +0000

    Case insensitive commands
    Changed the config to allow for case insensitive commands to be enabled or disabled

--- runbot.py
+++ runbot.py
@@ -30,7 +30,7 @@ class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        super().__init__(command_prefix = "!", intents = intents, case_insensitive=True)
+        super().__init__(command_prefix=cfg.prefix, intents = intents, case_insensitive=cfg.commands_case_insensitive)
 
     async def setup_hook(self):
         event.set()
